003e07ca0afc61fc6ef8536c5e139212caa164f4
son <handsun000@gmail.com>
2025-02-20 14:40:42 +0900

    work

--- backend/src/main/kotlin/com/ll/domain/member/member/dto/MemberWithUsernameDto.kt
+++ backend/src/main/kotlin/com/ll/domain/member/member/dto/MemberWithUsernameDto.kt
@@ -1,10 +1,11 @@
 package com.ll.domain.member.member.dto;
 
 import com.ll.domain.member.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.lang.NonNull;
 
 class MemberWithUsernameDto(
-    member:Member
+    @Schema(hidden = true) private val member:Member
 ) : MemberDto(member) {
     @NonNull
     val username: String = member.username
